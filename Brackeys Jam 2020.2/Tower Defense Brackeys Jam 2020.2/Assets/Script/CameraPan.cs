ae8435bd9f23380f2e1914f22130ee03177079c2
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-08-01 09:08:26 -0500

    camera

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/CameraPan.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    CharacterController controller;
+
+    void Start() => controller = GetComponent<CharacterController>();
+
+    void Update() => Move();
+
+    void Move()
+    {
+        controller.Move(new Vector3(Input.GetAxisRaw("Vertical") * -1f, 0f, Input.GetAxisRaw("Horizontal")) * moveSpeed * Time.deltaTime);
+    }
+}
