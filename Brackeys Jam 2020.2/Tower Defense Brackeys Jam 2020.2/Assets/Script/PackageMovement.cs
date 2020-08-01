d3c7bde714313dba356b78769e179f29bb4842a8
Angela Lam <angelakam.lam@gmail.com>
2020-08-01 06:26:46 -0700

    Update movement script
    Changed EnemyMovement to PackageMovement because it applies to any incoming package.

--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/PackageMovement.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/PackageMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class PackageMovement : MonoBehaviour
 {
     [SerializeField]
     float speed = 5;
