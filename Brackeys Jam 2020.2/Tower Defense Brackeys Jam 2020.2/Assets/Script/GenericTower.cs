cf40384034de4e3bea0b229896c4a403e0e0ec43
RedFusion Design <jan.wiechers04@gmail.com>
2020-08-01 14:31:03 +0200

    Started Basic Shooting Mechanic

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/GenericTower.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTower : MonoBehaviour
+{
+    float Range = 10;
+    string TargetTag = "Package";
+}
---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/Target.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    List<int> CheckedBy = new List<int>(); 
+    bool IsBad = false;
+    int AttackID = 0;
+    int Health = 100;
+
+
+    public void AddToList(int TowerID)
+    {
+        CheckedBy.Add(TowerID);
+    }
+
+    public int[] GetIDList()
+    {
+        return CheckedBy.ToArray();
+    }
+    public void Damage(int Amount)
+    {
+        Health -= Amount;
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool GetFlagg()
+    {
+        return IsBad;
+    }
+
+    public void SetFlagg(bool State)
+    {
+        IsBad = State;
+    }
+
+    public int GetID()
+    {
+        return AttackID;
+    }
+}
