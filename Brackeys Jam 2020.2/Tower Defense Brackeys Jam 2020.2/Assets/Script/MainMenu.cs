0851d0e08f50c6d80437450a0a740373a21c8a9e
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-08-01 16:29:59 -0500

    main menu

--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/MainMenu.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/MainMenu.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+    public void Play() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     public void Quit()
     {
