1f11c8985a0af392963bbb48efe8903d138e66d6
Angela Lam <angelakam.lam@gmail.com>
2020-08-01 13:12:42 -0700

    Create Enemy Class
    Consolidate package class
    Create enemy class

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/Package/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : PackageClass
+{
+    [SerializeField] private string enemyName;
+
+    private float hp;
+
+    [SerializeField] private float maxHP;
+
+    private void Start()
+    {
+        hp = maxHP;
+        Intro();
+    }
+    private void Intro()
+    {
+        Debug.Log(enemyName + ", " + hp);
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if (Vector3.Distance(transform.position, wp.waypoints[wpIndex].position) < 0.1f)
+        {
+            if (wpIndex < wp.waypoints.Length - 1)
+            {
+                wpIndex++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/PackageClass.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public class PackageClass : MonoBehaviour
-{
-    public bool isBad;
-}
