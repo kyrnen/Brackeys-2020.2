c91122d920c73879c50d1016fde4a1371b227250
guy196 <guy.shalev01@gmail.com>
2020-08-02 23:06:22 +0300

    a
    a

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUi;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        PauseMenuUi.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUi.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+}
