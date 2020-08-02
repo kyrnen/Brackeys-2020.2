3992871d34d52e09a30901c40d55ea3e1bdc71e7
guy196 <guy.shalev01@gmail.com>
2020-08-03 02:03:57 +0300

    a
    a

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/BillBoard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    public Transform cam;
+    void FixedUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
