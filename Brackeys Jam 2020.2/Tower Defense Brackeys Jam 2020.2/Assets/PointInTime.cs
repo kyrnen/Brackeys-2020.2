62d9794d6f41dc89ac093b07e4b6d677eb904a0e
guy196 <guy.shalev01@gmail.com>
2020-08-03 22:43:53 +0300

    a
    a

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/PointInTime.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PointInTime
+{
+	public Vector3 Poition;
+	public Quaternion rotation;
+
+	public PointInTime (Vector3 _position, Quaternion _rotation)
+	{
+		Poition = _position;
+		rotation = _rotation;
+	}
+}
---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/RewindTime.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class RewindTime : MonoBehaviour
+{
+    private bool isRewinding = false;
+    Rigidbody rb;
+    List<PointInTime> PointInTime;
+    void Start()
+    {
+        PointInTime = new List<PointInTime>();
+        }
+
+    void FixedUpdate()
+    {
+        if (isRewinding)
+            Rewind();
+        else
+            Record();
+    }
+
+    void Record()
+    {
+        if(PointInTime.Count > Mathf.Round(5f / Time.fixedDeltaTime))
+        {
+            PointInTime.RemoveAt(PointInTime.Count - 1);
+        }
+        PointInTime.Insert(0, new PointInTime(transform.position,transform.rotation));
+    } 
+    void Rewind()
+    {
+        if (PointInTime.Count > 0)
+        {
+            PointInTime pointInTime = PointInTime[0];
+            transform.position = pointInTime.Poition;
+            transform.rotation = pointInTime.rotation;
+            PointInTime.RemoveAt(0);
+        }
+        else
+            StopRewind();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartRewind();
+        }
+    }
+    void StartRewind()
+    {
+        isRewinding = true;
+    }
+    void StopRewind()
+    {
+        isRewinding = false;
+    }
+}
