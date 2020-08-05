0986e823a597589637de45adff572c68a448bd06
guy196 <guy.shalev01@gmail.com>
2020-08-05 17:59:52 +0300

    Revert "aa"
    This reverts commit 5257d41f93f61dc1bbf6d5c17084cfd41e908764.

--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/BillBoard.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/BillBoard.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class BillBoard : MonoBehaviour
 {
     public Transform cam;
-    public void Start()
-    {
-        cam = Camera.main.transform;
-    }
     void FixedUpdate()
     {
         transform.LookAt(transform.position + cam.forward);
--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/Node.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/Node.cs
@@ -1,42 +1,40 @@
 ﻿using UnityEngine;
-/*
 
 public class Node : MonoBehaviour
 {
-public Color hoverColor;
-public Vector3 positionOffset;
-public GameObject stand;
-private GameObject turret;
-private Renderer rend;
-private Color startColor;
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    public GameObject stand;
+    private GameObject turret;
 
-void Start()
-{
-    rend = GetComponent<Renderer>();
-    startColor = rend.material.color;
-}
+    private Renderer rend;
+    private Color startColor;
 
-void OnMouseDown()
-{
-    if(turret != null)
+    void Start()
     {
-        Debug.Log("Can't build there!");
-        return;
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
     }
-    stand.SetActive(true);
-    GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
-    turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
-}
 
-void OnMouseEnter()
-{
-    rend.material.color = hoverColor;
-}
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there!");
+            return;
+        }
+        stand.SetActive(true);
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
 
-void OnMouseExit()
-{
-    rend.material.color = startColor;
-}
-}
-    */
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
 
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
