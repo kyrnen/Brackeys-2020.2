3fa706e78334aa5684ccbb2e477051b6fa5f4d39
ieatrice69420 <65869139+ieatrice69420@users.noreply.github.com>
2020-08-05 07:41:23 -0500

    player health

--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/PlayerHealth.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/PlayerHealth.cs
@@ -1,14 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public static PlayerHealth instance;
+
     [SerializeField]
     float health = 100f;
+
+    [SerializeField]
+    Slider healthBar;
+
+    [SerializeField]
+    Gradient healthColor;
+
+    [SerializeField]
+    Image fill;
+
+    void Awake() => instance = this;
+
+    void Start()
+    {
+        SetHealthBar();
+    }
+
 
     public void TakeDamage(float amnt)
     {
         health -= amnt;
+        SetHealthBar();
     }
 
     void Update()
@@ -19,5 +40,11 @@ public class PlayerHealth : MonoBehaviour
     void Respawn()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void SetHealthBar()
+    {
+        healthBar.value = health;
+        fill.color = healthColor.Evaluate(healthBar.normalizedValue);
     }
 }
