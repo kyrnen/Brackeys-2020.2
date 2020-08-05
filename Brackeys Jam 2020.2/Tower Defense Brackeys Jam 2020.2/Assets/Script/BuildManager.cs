69a4ef010368de9987397529e77aebe78dafd31a
guy196 <guy.shalev01@gmail.com>
2020-08-05 17:59:57 +0300

    Revert "aa"
    This reverts commit 9769638f061dba205e6c2a501f8026e3f486b080.

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private GameObject turretToBuild;
+    public GameObject standardTurretPrefab;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
