5257d41f93f61dc1bbf6d5c17084cfd41e908764
guy196 <guy.shalev01@gmail.com>
2020-08-05 17:59:37 +0300

    aa
    a

--- Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/BillBoard.cs
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/BillBoard.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BillBoard : MonoBehaviour
 {
     public Transform cam;
+    public void Start()
+    {
+        cam = Camera.main.transform;
+    }
     void FixedUpdate()
     {
         transform.LookAt(transform.position + cam.forward);
