9cd67581b0d5c63e618bcf01022073760ae3377e
guy196 <guy.shalev01@gmail.com>
2020-08-06 07:30:56 +0300

    Ready

---
+++ Brackeys Jam 2020.2/Tower Defense Brackeys Jam 2020.2/Assets/Script/ReadyToFight.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadyToFight : MonoBehaviour
+{
+    public bool ReadyToFightButton;
+    public Button texttodisable;
+
+    void Update()
+    {
+        if (ReadyToFightButton == true)
+        {
+            GetComponent<WaveManager>().enabled = true;
+        }
+    }
+    public void OnPressReadyToFightButton()
+    {
+        ReadyToFightButton = true;
+        texttodisable.enabled = false;
+    }
+}
